cebf02f790f1dc2276b5cdebe7ef83a45105b380
Gustavo Gomez <ggem@acm.org>
2015-02-15 07:12:34 -0700

    getting news articles from nytimes api

--- js/script.js
+++ js/script.js
@@ -17,9 +17,27 @@ function loadData() {
 
     $greeting.text('So, you want to live at ' + address + '?');
 
+    // load background image from street view
     var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address;
     $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
 
+    // load articles from New York Times
+    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key='
+    $.getJSON(nytimesUrl, function(data){
+
+        $nytHeaderElem.text('New York Times Articles About ' + cityStr);
+
+        articles = data.response.docs;
+        for (var i = 0; i < articles.length; i++) {
+            var article = articles[i];
+            $nytElem.append('<li class="article">'+
+                '<a href="'+article.web_url+'">'+article.headline.main+'</a>'+
+                '<p>' + article.snippet + '</p>'+
+            '</li>');
+        };
+    }).error(function(e){
+        $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
+    });
 
     return false;
 };
